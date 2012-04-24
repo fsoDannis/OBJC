8c43ffe17217fc65efa94de845c4c6738863f761
Dan <Dan@Dinkydetails.com>
2012-04-23 21:57:50 -0400

    Creating Project 4

---
+++ Project4/Project4/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Project4
+//
+//  Created by Annis Dan on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Project4/Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project4
+//
+//  Created by Annis Dan on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
