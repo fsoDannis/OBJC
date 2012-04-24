1a1db6a1605d23f090502cd51e37ab39f423eff6
Dan <Dan@Dinkydetails.com>
2012-04-23 22:03:05 -0400

    Setting up basic app - changing BG color and Adding info

---
+++ Project4/Project4/UICustomColors.h
@@ -0,0 +1,18 @@
+@interface UIColor (Custom)
+
++ (UIColor *)myRed;
++ (UIColor *)mylightRed;
++ (UIColor *)myOrange;
++ (UIColor *)mylightOrange;
++ (UIColor *)myYellow;
++ (UIColor *)mylightYellow;
++ (UIColor *)myGreen;
++ (UIColor *)mylightGreen;
++ (UIColor *)myBlue;
++ (UIColor *)mylightBlue;
++ (UIColor *)myIndigo;
++ (UIColor *)mylightIndigo;
++ (UIColor *)myViolet;
++ (UIColor *)mylightViolet;
+
+@end
--- Project4/Project4/ViewController.m
+++ Project4/Project4/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#include "UICustomColors.h"
 
 @interface ViewController ()
 
@@ -19,6 +20,34 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    
+    // SETTING BACKGROUND COLOR
+    self.view.backgroundColor=[UIColor blackColor];
+    
+    // LABEL FOR MY INFORMATION
+    projectInfo = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 395.0f, 300.0f,50.0f)];
+    [self.view addSubview:projectInfo];
+    
+    [super viewWillAppear:animated];
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    
+    // ADDING IN SOME PROJECT INFORMATION    
+    projectInfo.text=@"Dan Annis - 4/19/2012 - Project 4";
+    projectInfo.textAlignment=UITextAlignmentCenter;
+    projectInfo.backgroundColor=[UIColor myBlue];
+    projectInfo.textColor= [UIColor whiteColor];
+    [super viewDidAppear:animated];
+    
+
+}
+
 
 - (void)viewDidUnload
 {
