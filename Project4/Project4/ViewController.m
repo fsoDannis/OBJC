804f16a17e1b3dfa791dfcb5c91891f9cd9b1c5d
Dan <Dan@Dinkydetails.com>
2012-04-23 22:36:33 -0400

    Created some Buttons

--- Project4/Project4/ViewController.m
+++ Project4/Project4/ViewController.m
@@ -17,6 +17,38 @@
 
 - (void)viewDidLoad
 {
+    // JUST A BUTTON
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if(button !=nil)
+    {
+        button.frame = CGRectMake(10.0f, 10.0f, 100.0f, 50.0f);
+        button.tintColor= [UIColor  mylightGreen];
+        [button setTitle: @"Push Me" forState:UIControlStateNormal];
+        [button setTitle: @"Pushed" forState:UIControlStateHighlighted];
+        
+        [self.view addSubview:button];
+    }    
+    // GOOD FRAME FOR UI BUTTONS  
+    UIButton *infoButton =  [UIButton buttonWithType:UIButtonTypeInfoLight]; 
+    if(infoButton !=nil)
+    {
+       // infoButton.backgroundColor = [UIColor myGreen]; //Used to test the Image frame
+        infoButton.frame = CGRectMake(290.0f, 10.0f, 22.0f, 22.0f);
+        [self.view addSubview:infoButton];
+    }
+    
+    //CUSTOM MAGE BUTTON - Don't forget to add the image to the list
+    UIButton *customButton =  [UIButton buttonWithType:UIButtonTypeCustom]; 
+    if(customButton !=nil)
+    {
+        UIImage *myCustomImage = [UIImage imageNamed:@"customButton.png"];
+        [customButton setImage:myCustomImage forState:UIControlStateNormal];
+        customButton.frame = CGRectMake(0.0f, 350.0f, 320.0f, 50.0f);
+        [self.view addSubview:customButton];
+    }
+    
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
